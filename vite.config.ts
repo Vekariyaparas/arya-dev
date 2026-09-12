5f489c231967d5105479f6c33f7ecc4601eef18c
Paras Vekariya <paras@tuvoc.com>
2026-09-12 15:26:27 +0530

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/aryadev/",
-  plugins: [react()]
+  base: "/arya-dev/",
+  plugins: [react()],
 });
