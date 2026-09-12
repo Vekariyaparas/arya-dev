43e6c7eae72f161b71eaff12e45cbf5be817d7ea
Paras Vekariya <paras@tuvoc.com>
2026-09-12 15:13:12 +0530

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,7 @@
-import tailwindcss from '@tailwindcss/vite'
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {
-    proxy: {
-      '/api': 'http://localhost:8787',
-    },
-  },
-})
+  base: "/aryadev/",
+  plugins: [react()]
+});
